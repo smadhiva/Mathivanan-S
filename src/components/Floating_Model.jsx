bf8e8bc038bc0e7990e20d39b82b1a8488f9a11a
smadhiva <smadhivanan1234@gmail.com>
2025-05-22 14:39:34 +0530

    deploy5

--- src/components/Floating_Model.jsx
+++ src/components/Floating_Model.jsx
@@ -3,7 +3,7 @@ import { useGLTF } from '@react-three/drei';
 import * as THREE from 'three';
 
 const Floating_Model = () => {
-   const gltf = useGLTF('../../public/models/crystal_planet.glb');
+   const gltf = useGLTF('/models/crystal_planet.glb');
   const ref = useRef();
 
   useEffect(() => {
